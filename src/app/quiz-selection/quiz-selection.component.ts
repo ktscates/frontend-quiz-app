89878c87643546703b1611d78c298a29ccd5168b
ktscates <cates2506@gmail.com>
2024-07-02 15:08:17 +0200

    project setup

---
+++ src/app/quiz-selection/quiz-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-selection.component.html',
+  styleUrl: './quiz-selection.component.css'
+})
+export class QuizSelectionComponent {
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#A729F5",
+        darkNavy: "#313E51",
+        navy: "#3B4D66",
+        greyNavy: "#626C7F",
+        lightGrey: "#F4F6FA",
+        lightBlue: "#ABC1E1",
+        green: "#26D782",
+        red: "#EE5454",
+      },
+      fontFamily: {
+        primary: ["Rubik", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
