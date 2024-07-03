f9c537b16dd5d9a8b43356480e34736742837086
ktscates <cates2506@gmail.com>
2024-07-03 15:42:08 +0200

    quiz selection ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-
+import { QuizSelectionComponent } from './quiz-selection/quiz-selection.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, QuizSelectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
